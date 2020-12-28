127e8471a5caf678a7737635560e7b4fa03e819f
laonahongchen <chenweizhe1999@126.com>
2020-12-28 13:04:33 +0800

    add new tag sample

--- run_tag_sample.py
+++ run_tag_sample.py
@@ -1,7 +1,6 @@
 # from env.matrix_env import MatrixEnv
 from env.tagging import TaggingEnv
-# from env.sec_ac import SecurityEnv
-from sample_ac_controller import NaiveController
+from npa_controller_torch import NaiveController
 # import seaborn as sns
 # import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,21 +18,21 @@ pac_agent_cnt = 0
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Run security game.")
+    parser = argparse.ArgumentParser(description="Run tagging game.")
 
     parser.add_argument('--agent', type=str, default="ppo")
-    parser.add_argument('--episodes', type=int, default=1000)
-    parser.add_argument('--n-steps', type=int, default=1)
+    parser.add_argument('--episodes', type=int, default=1)
+    parser.add_argument('--n-steps', type=int, default=20)
+    parser.add_argument('--train-round', type=int, default=0)
+    parser.add_argument('--train-belief', type=int, default=0)
     parser.add_argument('--n-belief', type=int, default=10)
-    parser.add_argument('--n-states', type=int, default=10)
+    parser.add_argument('--max-process', type=int, default=2)
     parser.add_argument('--steps-per-round', type=int, default=5)
     parser.add_argument('--prior', type=float, nargs='+', default=[0.5, 0.5])
-    parser.add_argument('--learning-rate', type=float, default=1e-3)
+    parser.add_argument('--learning-rate', type=float, default=5e-4)
     parser.add_argument('--batch-size', type=int, default=1000)
     parser.add_argument('--minibatch', type=int, default=100)
-    parser.add_argument('--test-every', type=int, default=5)
-    parser.add_argument('--k-epochs', type=int, default=50)
-    parser.add_argument('--v-epochs', type=int, default=100)
+    parser.add_argument('--test-every', type=int, default=100)
     parser.add_argument('--save-every', type=int)
     parser.add_argument('--load', action="store_true")
     parser.add_argument('--random-prior', action="store_true")
@@ -46,7 +45,11 @@ def parse_args():
     parser.add_argument('--iterations-per-round', type=int, default=16)
     parser.add_argument('--exp-name', type=str)
     parser.add_argument('--other', type=str, default='')
-    parser.add_argument('--seed', type=int, default=6657)
+    parser.add_argument('--seed', type=int, default=377)
+    parser.add_argument('--k-epochs', type=int, default=1000)
+    parser.add_argument('--v-batch-size', type=int, default=100000)
+    parser.add_argument('--num-thread', type=int, default=5)
+    parser.add_argument('--total-process', type=int, default=64)
 
     return parser.parse_args()
 
@@ -68,7 +71,6 @@ if __name__ == "__main__":
     n_steps = args.n_steps
     steps_per_round = args.steps_per_round
     prior = args.prior
-    # prior = [0.2] * 5
     lr = learning_rate = args.learning_rate
     # schedule = ("wolf_adv", 20.0)
     test_every = args.test_every
@@ -80,13 +82,11 @@ if __name__ == "__main__":
     network_depth = args.network_depth
     timesteps_per_batch = args.timesteps_per_batch
     iterations_per_round = args.iterations_per_round
-    betas = [[0, 0.999], [0, 0.99]]
+    betas = [[0.9, 0.99], [0.9, 0.99]]
     gamma = 0.95
     max_episodes = args.episodes
     clip_eps = 0.2
     n_belief = args.n_belief
-
-    priors = [[0.1 * i, 1 - 0.1 * i] for i in range(3, 8)]
 
     # other = "1000-test-steps-large-network"
 
@@ -110,53 +110,10 @@ if __name__ == "__main__":
 
     res = {"episode": [], "current_assessments": [], "player": []}
 
-    tot_res = []
+    env = TaggingEnv(n_steps=n_steps, prior=prior)
+    # env.export_payoff("/home/footoredo/playground/REPEATED_GAME/EXPERIMENTS/PAYOFFSATTvsDEF/%dTarget/inputr-1.000000.csv" % n_slots)
+    if train:
+        controller = NaiveController(env, max_episodes, lr, betas, gamma, clip_eps, n_steps, network_width, test_every, n_belief, args.batch_size, args.minibatch, k_epochs=args.k_epochs,max_process=args.max_process, v_batch_size=args.v_batch_size, total_process=args.total_process, seed = args.seed)
+        controller.sample_belief(args.train_round, args.train_belief, max_steps, args.exp_name)
 
-    for i in range(1):
-
-        env = TaggingEnv(n_steps=n_steps, prior=prior, seed=args.seed + i)
-
-        for prior in priors:
-            print('start with prior:')
-            print(prior)
-
-            env.set_prior(prior)
-
-            # env.export_payoff("/home/footoredo/playground/REPEATED_GAME/EXPERIMENTS/PAYOFFSATTvsDEF/%dTarget/inputr-1.000000.csv" % n_slots)
-            if train:
-                controller = NaiveController(env, max_episodes, lr, betas, gamma, clip_eps, n_steps, network_width, test_every, n_belief, args.n_states, args.batch_size, args.minibatch, args.k_epochs, args.v_epochs, args.seed)
-                controller.train(num_round=1, round_each_belief = max_steps)
-
-                
-                print('train finish')
-                controller.assess_strategy(1000)
-
-                # strategies = controller.agents[0], controller.agents[1]
-                # tot_res.append(env.assess_strategies(strategies))
-                print(tot_res)
-    
-    print(tot_res)
-
-# 6657 5410 5748 9418 9527
-
-
-# 21/sec_2: 2 3: 4.2156
-# 21/sec: 2 5: 7.7078
-# 14/npa_2: 2 8: 20.3159
-
-'''
- 5 5： 
-
-npa:
-PBNE: [tensor(6.8976, grad_fn=<SubBackward0>), tensor(15.9111, grad_fn=<SubBackward0>)] tensor(8.0222, grad_fn=<RsubBackward1>)                                                                                                                 BR: [tensor(22.4417, grad_fn=<AddBackward0>), tensor(31.7153, grad_fn=<AddBackward0>)] -6.942926804836428               Overall: [tensor(6.6375, grad_fn=<AddBackward0>), tensor(15.9111, grad_fn=<AddBackward0>)] tensor(8.0222, grad_fn=<RsubBackward1>)   
-sec: 
-PBNE: [tensor(9.7253, device='cuda:2', grad_fn=<SubBackward0>), tensor(11.2245, device='cuda:2', grad_fn=<SubBackward0>)] tensor(1.8543, device='cuda:2', grad_fn=<RsubBackward1>)                                                              BR: [tensor(30.9328, device='cuda:2', grad_fn=<AddBackward0>), tensor(33.0172, device='cuda:2', grad_fn=<AddBackward0>)] -19.62590601456302                                                                                                     Overall: [tensor(9.7253, device='cuda:2', grad_fn=<AddBackward0>), tensor(11.2245, device='cuda:2', grad_fn=<AddBackward0>)] tensor(1.8543, device='cuda:2', grad_fn=<RsubBackward1>)  
-
-
-5 10:
-21/sec_2: npa: 
-
-10 10:
-
-12/npa
-'''
+        # controller.assess_strategy()
